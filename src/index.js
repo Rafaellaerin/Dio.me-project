cd2307779e5aed7d0b2cf6c49d8f78691e3438db
Rafael Laerin <laerinrusso@gmail.com>
2024-12-18 19:48:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,26 @@
-let nomesHerois = ["Thor", "Iron Man", "Hulk", "Black Widow", "Captain America"];
-let xpsHerois = [7500, 1500, 8500, 300, 12000];
+let heroi = "Homem Aranha";
+let xp = 2300;
+let nivel = "";
 let i = 0;
-
-while (i < nomesHerois.length) {
-    let nomeHeroi = nomesHerois[i];
-    let xpHeroi = xpsHerois[i];
-    let nivelHeroi;
-
-    if (xpHeroi < 1000) {
-        nivelHeroi = "Ferro";
-    } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-        nivelHeroi = "Bronze";
-    } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-        nivelHeroi = "Prata";
-    } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
-        nivelHeroi = "Ouro";
-    } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-        nivelHeroi = "Platina";
-    } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-        nivelHeroi = "Ascendente";
-    } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-        nivelHeroi = "Imortal";
-    } else {
-        nivelHeroi = "Radiante";
+while (i < 1){
+    if (xp < 1000){
+        nivel = "Ferro";
+    } else if(xp >= 1001 && xp <= 2000){
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000){
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000){
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000){
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000){
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000){
+        nivel = "Imortal";
+    } else if(xp >= 10001){
+        nivel = "Radiante";
     }
-
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi);
-    i++;
+    
+    i++
 }
-
-do {
-    let nomeHeroi = nomesHerois[i - 1];
-    let xpHeroi = xpsHerois[i - 1];
-    console.log("Reprocessando o Herói: " + nomeHeroi + " com XP: " + xpHeroi);
-    i--;
-} while (i > 0);
-
-for (let j = 0; j < nomesHerois.length; j++) {
-    let nomeHeroi = nomesHerois[j];
-    console.log("Confirmado: O Herói " + nomeHeroi + " foi classificado com sucesso.");
-}
-
-switch (xpsHerois[0]) {
-    case 7500:
-        console.log("O XP do herói Thor está no intervalo de Ouro.");
-        break;
-    default:
-        console.log("O XP não foi reconhecido.");
-        break;
-}
+console.log("O Herói de nome " + heroi + " está no nivel de " + nivel);
